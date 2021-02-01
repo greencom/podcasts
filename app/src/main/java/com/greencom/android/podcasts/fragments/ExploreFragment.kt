370a62fd1e8ff74143c026976da1d8d3bfabdc71
Artyom <s.artem.ekb@gmail.com>
2021-02-01 17:29:42 +0300

    Transfer update

--- app/src/main/java/com/greencom/android/podcasts/fragments/ExploreFragment.kt
+++ app/src/main/java/com/greencom/android/podcasts/fragments/ExploreFragment.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.navGraphViewModels
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import com.greencom.android.podcasts.R
 import com.greencom.android.podcasts.databinding.FragmentExploreBinding
 import com.greencom.android.podcasts.viewmodels.ExploreViewModel
+
+private const val NUM_PAGES = 5
 
 class ExploreFragment : Fragment() {
 
@@ -26,6 +32,20 @@ class ExploreFragment : Fragment() {
     ): View {
         /** ExploreFragment View binding setup. */
         _binding = FragmentExploreBinding.inflate(inflater, container, false)
+
+        val pagerAdapter = PagerAdapter(childFragmentManager, lifecycle)
+        binding.pager.adapter = pagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "For You"
+                1 -> tab.text = "News"
+                2 -> tab.text = "Culture"
+                3 -> tab.text = "Education"
+                4 -> tab.text = "Business"
+            }
+        }.attach()
+
         return binding.root
     }
 
@@ -39,5 +59,20 @@ class ExploreFragment : Fragment() {
         super.onDestroyView()
         // Clear ExploreFragment View binding.
         _binding = null
+    }
+
+    private inner class PagerAdapter(
+            fm: FragmentManager,
+            lifecycle: Lifecycle
+    ) : FragmentStateAdapter(fm, lifecycle) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment {
+            return if (position == 0) {
+                ExplorePrimaryPageFragment()
+            } else {
+                ExploreSecondaryPageFragment()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/greencom/android/podcasts/fragments/ExplorePrimaryPageFragment.kt
@@ -0,0 +1,32 @@
+package com.greencom.android.podcasts.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.greencom.android.podcasts.R
+import com.greencom.android.podcasts.databinding.FragmentExplorePrimaryPageBinding
+
+class ExplorePrimaryPageFragment : Fragment() {
+
+    /** Nullable ExplorePrimaryPageFragment View binding. Use [binding] instead. */
+    private var _binding: FragmentExplorePrimaryPageBinding? = null
+    /** Non-null ExplorePrimaryPageFragment View binding. */
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        /** ExplorePrimaryPageFragment View binding setup. */
+        _binding = FragmentExplorePrimaryPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Clear ExplorePrimaryPageFragment View binding.
+        _binding = null
+    }
+}
