0be6098ce81ec5585e7e212d7d2534685f170734
Artyom <s.artem.ekb@gmail.com>
2021-02-04 22:18:58 +0300

    Add Event class

---
+++ app/src/main/java/com/greencom/android/podcasts/utils/Event.kt
@@ -0,0 +1,61 @@
+package com.greencom.android.podcasts.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ */
+class Event<out T>(private val content: T) {
+
+    /** Whether the event has been handled. */
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /** Returns the content and prevents its use again. */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /** Returns the content, even if it has already been handled. */
+    fun peekContent(): T = content
+}
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been handled.
+ *
+ * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
+ *
+ * Alternative for the [observeEvent] method.
+ *
+ * Usage:
+ * `liveData.observe(lifecycleOwner, EventObserver {  })`
+ */
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let(onEventUnhandledContent)
+    }
+}
+
+/**
+ * Used as replacement for the default [LiveData.observe] method to observing [Event]'s.
+ * Simplifying the pattern of checking if the [Event]'s content has already been handled.
+ *
+ * Alternative for the [EventObserver] class.
+ *
+ * Usage:
+ * `liveData.observeEvent(lifecycleOwner) {  }`
+ */
+inline fun <T> LiveData<Event<T>>.observeEvent(
+        owner: LifecycleOwner,
+        crossinline onEventUnhandledContent: (T) -> Unit
+) {
+    observe(owner) { it?.getContentIfNotHandled()?.let(onEventUnhandledContent) }
+}
