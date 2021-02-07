ad70bf37ffdd8d9c62d3e5551105f3b6da1664c3
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-02-07 16:28:33 +0300

    Add Timber for better logging

--- app/src/main/java/com/greencom/android/podcasts/PodcastsApplication.kt
+++ app/src/main/java/com/greencom/android/podcasts/PodcastsApplication.kt
@@ -2,6 +2,17 @@ package com.greencom.android.podcasts
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class PodcastsApplication : Application()
+class PodcastsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Timber initialization.
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
