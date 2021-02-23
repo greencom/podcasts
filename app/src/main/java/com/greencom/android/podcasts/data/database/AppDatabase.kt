ad5754df43f377930911bcd494bab4588552512d
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-02-23 17:43:35 +0300

    Improvements and cleaning

--- app/src/main/java/com/greencom/android/podcasts/data/database/AppDatabase.kt
+++ app/src/main/java/com/greencom/android/podcasts/data/database/AppDatabase.kt
@@ -3,6 +3,7 @@ package com.greencom.android.podcasts.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+/** App database that provides DAO objects to get access to the database tables. */
 @Database(entities = [PodcastEntity::class, EpisodeEntity::class, GenreEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
