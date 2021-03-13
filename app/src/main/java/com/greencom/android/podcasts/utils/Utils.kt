59a67daba010bb00ef61400a479298127a101922
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-03-13 16:38:52 +0300

    Add global tag for logging

--- app/src/main/java/com/greencom/android/podcasts/utils/Utils.kt
+++ app/src/main/java/com/greencom/android/podcasts/utils/Utils.kt
@@ -2,6 +2,9 @@ package com.greencom.android.podcasts.utils
 
 import android.content.Context
 
+/** Global tag for logging. */
+const val GLOBAL_TAG = "GLOBAL_TAG"
+
 /** Convert `dp` to `px`. */
 fun Context.dpToPx(dp: Float): Float {
     return dp * resources.displayMetrics.density
