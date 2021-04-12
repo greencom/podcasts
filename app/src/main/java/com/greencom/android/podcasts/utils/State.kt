ecaba7f62f663204f053c0a69ec536e751358a67
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-04-12 23:53:06 +0300

    Make State sealed class generic

--- app/src/main/java/com/greencom/android/podcasts/utils/State.kt
+++ app/src/main/java/com/greencom/android/podcasts/utils/State.kt
@@ -4,11 +4,11 @@ package com.greencom.android.podcasts.utils
  * Sealed class that represents the state of a process. The state could be either [Loading],
  * [Success] or [Error].
  */
-sealed class State {
+sealed class State<out T> {
     /** Object that represents the `Loading` state. */
-    object Loading : State()
+    object Loading : State<Nothing>()
     /** Class that represents the `Success` state with [data]. */
-    data class Success<out T>(val data: T) : State()
+    data class Success<out T>(val data: T) : State<T>()
     /** Class that represents the `Error` state with [exception]. */
-    data class Error(val exception: Throwable) : State()
+    data class Error(val exception: Throwable) : State<Nothing>()
 }
