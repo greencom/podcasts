3faf1c4b0262b6d91dbbfd7df5f9f142c667889c
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-04-23 15:26:28 +0300

    Remove test code

--- app/src/main/java/com/greencom/android/podcasts/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/greencom/android/podcasts/ui/explore/ExploreFragment.kt
@@ -9,7 +9,6 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.greencom.android.podcasts.R
 import com.greencom.android.podcasts.databinding.FragmentExploreBinding
-import com.greencom.android.podcasts.ui.dialogs.UnsubscribeDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -37,11 +36,6 @@ class ExploreFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         // TabLayout and ViewPager2 setup.
         setupTabLayout()
-
-        // TODO: Test code
-        binding.searchButton.setOnClickListener {
-            UnsubscribeDialog().show(parentFragmentManager, UnsubscribeDialog.TAG)
-        }
     }
 
     override fun onDestroyView() {
