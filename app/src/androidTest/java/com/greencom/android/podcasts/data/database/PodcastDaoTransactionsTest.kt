d9f123a5a0d7947219245d723ea2d30c1a980f39
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-05-03 21:35:42 +0300

    Comment out podcast DAO transaction tests

--- app/src/androidTest/java/com/greencom/android/podcasts/data/database/PodcastDaoTransactionsTest.kt
+++ app/src/androidTest/java/com/greencom/android/podcasts/data/database/PodcastDaoTransactionsTest.kt
@@ -33,117 +33,120 @@ class PodcastDaoTransactionsTest {
     @Throws(IOException::class)
     fun closeDatabase() = database.close()
 
-    @Test
-    @Throws(Exception::class)
-    fun insertPartial_insertSinglePodcast_returnPodcast() = runBlocking {
-        // GIVEN: `podcasts` table does not contain the podcast.
+    // Tests are commented out since methods used by them marked as protected.
+    // Make them public to run tests.
 
-        // WHEN: Insert the podcast into the `podcasts` table.
-        val podcast = PodcastEntityPartial(
-            "one",
-            "one",
-            "one",
-            "one",
-            "one",
-            false,
-            100,
-            100L
-        )
-        podcastDao.insert(podcast)
-
-        // THEN: loaded is valid.
-        val loaded = podcastDao.getPodcast(podcast.id)
-        assertThat(loaded?.title).isEqualTo(podcast.title)
-        assertThat(loaded?.subscribed).isFalse()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertPartial_podcastsTableContainsPartOfList_insertMissingAndUpdateExisting() =
-        runBlocking {
-            // GIVEN: `podcasts` table contains the podcast.
-            val podcastOne = PodcastEntityPartial(
-                "one",
-                "one",
-                "one",
-                "one",
-                "one",
-                false,
-                100,
-                100L
-            )
-            podcastDao.insert(podcastOne)
-
-            // WHEN: Insert the podcasts into the `podcasts` table.
-            val newOne = podcastOne.copy(title = "updated")
-            val newTwo = podcastOne.copy("two", "two")
-            val newThree = podcastOne.copy("three", "three")
-            podcastDao.insert(listOf(newOne, newTwo, newThree))
-
-            // THEN: All podcasts are valid.
-            val loadedOne = podcastDao.getPodcast(podcastOne.id)
-            val loadedTwo = podcastDao.getPodcast(newTwo.id)
-            val loadedThree = podcastDao.getPodcast(newThree.id)
-            assertThat(loadedOne?.title).isEqualTo(newOne.title)
-            assertThat(loadedTwo?.title).isEqualTo(newTwo.title)
-            assertThat(loadedThree?.title).isEqualTo(newThree.title)
-        }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertPartialWithGenre_insertSinglePodcast_returnPodcast() = runBlocking {
-        // GIVEN: `podcasts` table does not contain the podcast.
-
-        // WHEN: Insert the podcast into the `podcasts` table.
-        val podcast = PodcastEntityPartialWithGenre(
-            "one",
-            "one",
-            "one",
-            "one",
-            "one",
-            false,
-            100,
-            100L,
-            15
-        )
-        podcastDao.insertWithGenre(podcast)
-
-        // THEN: loaded is valid.
-        val loaded = podcastDao.getPodcast(podcast.id)
-        assertThat(loaded?.title).isEqualTo(podcast.title)
-        assertThat(loaded?.subscribed).isFalse()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun insertPartialWithGenre_podcastsTableContainsPartOfList_insertMissingAndUpdateExisting() =
-        runBlocking {
-            // GIVEN: `podcasts` table contains the podcast.
-            val podcastOne = PodcastEntityPartialWithGenre(
-                "one",
-                "one",
-                "one",
-                "one",
-                "one",
-                false,
-                100,
-                100L,
-                11
-            )
-            podcastDao.insertWithGenre(podcastOne)
-
-            // WHEN: Insert the podcasts into the `podcasts` table.
-            val newOne = podcastOne.copy(title = "updated")
-            val newTwo = podcastOne.copy("two", "two")
-            val newThree = podcastOne.copy("three", "three")
-            podcastDao.insertWithGenre(listOf(newOne, newTwo, newThree))
-
-            // THEN: All podcasts are valid.
-            val loadedOne = podcastDao.getPodcast(podcastOne.id)
-            val loadedTwo = podcastDao.getPodcast(newTwo.id)
-            val loadedThree = podcastDao.getPodcast(newThree.id)
-            assertThat(loadedOne?.title).isEqualTo(newOne.title)
-            assertThat(loadedTwo?.title).isEqualTo(newTwo.title)
-            assertThat(loadedThree?.title).isEqualTo(newThree.title)
-        }
+//    @Test
+//    @Throws(Exception::class)
+//    fun insertPartial_insertSinglePodcast_returnPodcast() = runBlocking {
+//        // GIVEN: `podcasts` table does not contain the podcast.
+//
+//        // WHEN: Insert the podcast into the `podcasts` table.
+//        val podcast = PodcastEntityPartial(
+//            "one",
+//            "one",
+//            "one",
+//            "one",
+//            "one",
+//            false,
+//            100,
+//            100L
+//        )
+//        podcastDao.insert(podcast)
+//
+//        // THEN: loaded is valid.
+//        val loaded = podcastDao.getPodcast(podcast.id)
+//        assertThat(loaded?.title).isEqualTo(podcast.title)
+//        assertThat(loaded?.subscribed).isFalse()
+//    }
+//
+//    @Test
+//    @Throws(Exception::class)
+//    fun insertPartial_podcastsTableContainsPartOfList_insertMissingAndUpdateExisting() =
+//        runBlocking {
+//            // GIVEN: `podcasts` table contains the podcast.
+//            val podcastOne = PodcastEntityPartial(
+//                "one",
+//                "one",
+//                "one",
+//                "one",
+//                "one",
+//                false,
+//                100,
+//                100L
+//            )
+//            podcastDao.insert(podcastOne)
+//
+//            // WHEN: Insert the podcasts into the `podcasts` table.
+//            val newOne = podcastOne.copy(title = "updated")
+//            val newTwo = podcastOne.copy("two", "two")
+//            val newThree = podcastOne.copy("three", "three")
+//            podcastDao.insert(listOf(newOne, newTwo, newThree))
+//
+//            // THEN: All podcasts are valid.
+//            val loadedOne = podcastDao.getPodcast(podcastOne.id)
+//            val loadedTwo = podcastDao.getPodcast(newTwo.id)
+//            val loadedThree = podcastDao.getPodcast(newThree.id)
+//            assertThat(loadedOne?.title).isEqualTo(newOne.title)
+//            assertThat(loadedTwo?.title).isEqualTo(newTwo.title)
+//            assertThat(loadedThree?.title).isEqualTo(newThree.title)
+//        }
+//
+//    @Test
+//    @Throws(Exception::class)
+//    fun insertPartialWithGenre_insertSinglePodcast_returnPodcast() = runBlocking {
+//        // GIVEN: `podcasts` table does not contain the podcast.
+//
+//        // WHEN: Insert the podcast into the `podcasts` table.
+//        val podcast = PodcastEntityPartialWithGenre(
+//            "one",
+//            "one",
+//            "one",
+//            "one",
+//            "one",
+//            false,
+//            100,
+//            100L,
+//            15
+//        )
+//        podcastDao.insertWithGenre(podcast)
+//
+//        // THEN: loaded is valid.
+//        val loaded = podcastDao.getPodcast(podcast.id)
+//        assertThat(loaded?.title).isEqualTo(podcast.title)
+//        assertThat(loaded?.subscribed).isFalse()
+//    }
+//
+//    @Test
+//    @Throws(Exception::class)
+//    fun insertPartialWithGenre_podcastsTableContainsPartOfList_insertMissingAndUpdateExisting() =
+//        runBlocking {
+//            // GIVEN: `podcasts` table contains the podcast.
+//            val podcastOne = PodcastEntityPartialWithGenre(
+//                "one",
+//                "one",
+//                "one",
+//                "one",
+//                "one",
+//                false,
+//                100,
+//                100L,
+//                11
+//            )
+//            podcastDao.insertWithGenre(podcastOne)
+//
+//            // WHEN: Insert the podcasts into the `podcasts` table.
+//            val newOne = podcastOne.copy(title = "updated")
+//            val newTwo = podcastOne.copy("two", "two")
+//            val newThree = podcastOne.copy("three", "three")
+//            podcastDao.insertWithGenre(listOf(newOne, newTwo, newThree))
+//
+//            // THEN: All podcasts are valid.
+//            val loadedOne = podcastDao.getPodcast(podcastOne.id)
+//            val loadedTwo = podcastDao.getPodcast(newTwo.id)
+//            val loadedThree = podcastDao.getPodcast(newThree.id)
+//            assertThat(loadedOne?.title).isEqualTo(newOne.title)
+//            assertThat(loadedTwo?.title).isEqualTo(newTwo.title)
+//            assertThat(loadedThree?.title).isEqualTo(newThree.title)
+//        }
 }
