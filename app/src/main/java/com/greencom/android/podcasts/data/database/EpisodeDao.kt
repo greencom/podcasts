1fa28f4a1e91c303808413e6b307a452810324ba
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-05-07 21:26:10 +0300

    Update EpisodeDao.kt

--- app/src/main/java/com/greencom/android/podcasts/data/database/EpisodeDao.kt
+++ app/src/main/java/com/greencom/android/podcasts/data/database/EpisodeDao.kt
@@ -1,7 +1,24 @@
 package com.greencom.android.podcasts.data.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 /** Interface to interact with the `episodes` table. */
 @Dao
-abstract class EpisodeDao
+abstract class EpisodeDao {
+
+    /**
+     * Insert an [EpisodeEntity] object into the database. [OnConflictStrategy.IGNORE]
+     * on conflict.
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insert(episode: EpisodeEntity)
+
+    /**
+     * Insert an [EpisodeEntity] list into the database. [OnConflictStrategy.IGNORE]
+     * on conflict.
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insert(episodes: List<EpisodeEntity>)
+}
