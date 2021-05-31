1bff6074904b6ad5b1ce6ac713372f3a2cb93a4f
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-05-31 21:06:27 +0300

    Update ImpossibleCaseException.kt

--- app/src/main/java/com/greencom/android/podcasts/utils/ImpossibleCaseException.kt
+++ app/src/main/java/com/greencom/android/podcasts/utils/ImpossibleCaseException.kt
@@ -1,6 +1,14 @@
 package com.greencom.android.podcasts.utils
 
 /** An exception indicates that an impossible case has occurred in the 'when' expression. */
-class ImpossibleCaseException(override val message: String) : Exception() {
-    constructor(): this("Impossible 'when' case")
+class ImpossibleCaseException(private val additionalMessage: String) : Exception() {
+
+    override val message: String
+        get() = if (additionalMessage.isBlank()) {
+            "Impossible 'when' case"
+        } else {
+            "Impossible 'when' case: $additionalMessage"
+        }
+
+    constructor(): this("")
 }
