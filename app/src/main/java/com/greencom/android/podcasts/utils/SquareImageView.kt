f82020f1ff6d1f0cbdad589d01a54a47dd0ab804
Artyom Sidorov <s.artem.ekb@gmail.com>
2021-08-10 17:35:14 +0300

    Update item_subscriptions_podcast_with_title.xml to use a LinearLayout with a SquareImageView

---
+++ app/src/main/java/com/greencom/android/podcasts/utils/SquareImageView.kt
@@ -0,0 +1,18 @@
+package com.greencom.android.podcasts.utils
+
+import android.content.Context
+import android.util.AttributeSet
+
+/** Square ImageView, the height of which is the same as the width. */
+class SquareImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+}
